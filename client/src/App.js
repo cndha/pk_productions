af10979afe8118e6e7c04a5ab5cbed5e4e743929
Cindy Ha <cindyhaa@gmail.com>
2021-12-21 13:26:47 -0800

    react front, node back

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get("/api")
+      .then(res => {
+        console.log(res);
+        setData(res.data.message)
+      })
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
